7781d870c7ab5ef20dab9e08873bc2fd9f6bc3ac
EderSampayo <101659210+EderSampayo@users.noreply.github.com>
2023-11-14 16:11:31 +0100

    Create suma.py

---
+++ suma.py
@@ -0,0 +1,10 @@
+def sumar_numeros(num1, num2):
+    suma = num1 + num2
+    return suma
+
+numero1 = 10
+numero2 = 5
+
+resultado_suma = sumar_numeros(numero1, numero2)
+
+print(f"La suma de {numero1} y {numero2} es: {resultado_suma}")
